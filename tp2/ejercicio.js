a5fa43b43222afc95cb8d5414fb137845bd96b3b
lauramuino <lauramuino2@gmail.com>
2021-05-02 03:52:15 -0300

    tp2

--- tp2/ejercicio.js
+++ tp2/ejercicio.js
@@ -11,7 +11,22 @@
 // Las rotaciones vienen expresadas en grados. 
 function BuildTransform( positionX, positionY, rotation, scale )
 {
-	return Array(1,0,0,0,1,0,0,0,1);
+
+	var matRot = Array( Math.cos(rotation), -Math.sin(rotation), 0,
+						Math.sin(rotation), Math.cos(rotation), 0,
+						0,              0,             1);
+
+	var matTras = Array(1, 0, 0,
+						0, 1, 0,
+						positionX, positionY, 1);
+
+	var matScale = Array(scale, 0 ,0,
+						 0, scale, 0,
+						 0, 0, 1);
+
+	var sScale = ComposeTransforms(matRot, (matTras));  //porque a la traslacion no hay que expresarla en column major?
+	var cScale = ComposeTransforms(transpose(matScale), sScale);
+	return cScale;
 }
 
 // Esta función retorna una matriz que resula de la composición de trasn1 y trans2. Ambas 
@@ -20,7 +35,29 @@ function BuildTransform( positionX, positionY, rotation, scale )
 // primero trans1 y luego trans2. 
 function ComposeTransforms( trans1, trans2 )
 {
-	return Array(1,0,0,0,1,0,0,0,1);
+	return transpose(composition (transpose(trans2), transpose(trans1)));
+}
+
+function composition(matrixA, matrixB){
+	var comp = Array(0,0,0,0,0,0,0,0,0);
+
+	for (var i = 0; i < 3; i++) {
+		for (var j = 0; j < 3; j++) {
+			var acum = 0;
+			for (var k = 0; k < 3; k++) {
+				acum = acum + matrixA[3*i+k] * matrixB[3*k+j];
+			}
+			comp[3*i+j] = acum;
+		}
+	}
+
+	return comp;
 }
 
 
+function transpose(matrix)
+{
+	return Array(matrix[0], matrix[3], matrix[6],
+				 matrix[1], matrix[4], matrix[7],
+				 matrix[2], matrix[5], matrix[8]);
+}
