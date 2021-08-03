91596e50f59470d7466ab6786b76b243281027b0
lauramuino <lauramuino2@gmail.com>
2021-08-03 18:09:52 -0300

    renombres e informe

--- tp-final/ejercicio.js
+++ tp-final/ejercicio.js
@@ -1,58 +1,59 @@
+/*
+ <============================================ EJERCICIOS ============================================>
+ a) Implementar la función:
 
-// <============================================ EJERCICIOS ============================================>
-// a) Implementar la función:
-//
-//      GetModelViewMatrix( translationX, translationY, translationZ, rotationX, rotationY )
-//
-//    Si la implementación es correcta, podrán hacer rotar la caja correctamente (como en el video). Notar 
-//    que esta función no es exactamente la misma que implementaron en el TP4, ya que no recibe por parámetro
-//    la matriz de proyección. Es decir, deberá retornar solo la transformación antes de la proyección model-view (MV)
-//    Es necesario completar esta implementación para que funcione el control de la luz en la interfaz. 
-//    IMPORTANTE: No es recomendable avanzar con los ejercicios b) y c) si este no funciona correctamente. 
-//
-// b) Implementar los métodos:
-//
-//      setMesh( vertPos, texCoords, normals )
-//      swapYZ( swap )
-//      draw( matrixMVP, matrixMV, matrixNormal )
-//
-//    Si la implementación es correcta, podrán visualizar el objeto 3D que hayan cargado, asi como también intercambiar 
-//    sus coordenadas yz. Notar que es necesario pasar las normales como atributo al VertexShader. 
-//    La función draw recibe ahora 3 matrices en column-major: 
-//
-//       * model-view-projection (MVP de 4x4)
-//       * model-view (MV de 4x4)
-//       * normal transformation (MV_3x3)
-//
-//    Estas últimas dos matrices adicionales deben ser utilizadas para transformar las posiciones y las normales del 
-//    espacio objeto al esapcio cámara. 
-//
-// c) Implementar los métodos:
-//
-//      setTexture( img )
-//      showTexture( show )
-//
-//    Si la implementación es correcta, podrán visualizar el objeto 3D que hayan cargado y su textura.
-//    Notar que los shaders deberán ser modificados entre el ejercicio b) y el c) para incorporar las texturas.
-//  
-// d) Implementar los métodos:
-//
-//      setLightDir(x,y,z)
-//      setShininess(alpha)
-//    
-//    Estas funciones se llaman cada vez que se modifican los parámetros del modelo de iluminación en la 
-//    interface. No es necesario transformar la dirección de la luz (x,y,z), ya viene en espacio cámara.
-//
-// Otras aclaraciones: 
-//
-//      * Utilizaremos una sola fuente de luz direccional en toda la escena
-//      * La intensidad I para el modelo de iluminación debe ser seteada como blanca (1.0,1.0,1.0,1.0) en RGB
-//      * Es opcional incorporar la componente ambiental (Ka) del modelo de iluminación
-//      * Los coeficientes Kd y Ks correspondientes a las componentes difusa y especular del modelo 
-//        deben ser seteados con el color blanco. En caso de que se active el uso de texturas, la 
-//        componente difusa (Kd) será reemplazada por el valor de textura. 
-//        
-// <=====================================================================================================>
+      GetModelViewMatrix( translationX, translationY, translationZ, rotationX, rotationY )
+
+    Si la implementación es correcta, podrán hacer rotar la caja correctamente (como en el video). Notar 
+    que esta función no es exactamente la misma que implementaron en el TP4, ya que no recibe por parámetro
+    la matriz de proyección. Es decir, deberá retornar solo la transformación antes de la proyección model-view (MV)
+    Es necesario completar esta implementación para que funcione el control de la luz en la interfaz. 
+    IMPORTANTE: No es recomendable avanzar con los ejercicios b) y c) si este no funciona correctamente. 
+
+ b) Implementar los métodos:
+
+      setMesh( vertPos, texCoords, normals )
+      swapYZ( swap )
+      draw( matrixMVP, matrixMV, matrixNormal )
+
+    Si la implementación es correcta, podrán visualizar el objeto 3D que hayan cargado, asi como también intercambiar 
+    sus coordenadas yz. Notar que es necesario pasar las normales como atributo al VertexShader. 
+    La función draw recibe ahora 3 matrices en column-major: 
+
+       * model-view-projection (MVP de 4x4)
+       * model-view (MV de 4x4)
+       * normal transformation (MV_3x3)
+
+    Estas últimas dos matrices adicionales deben ser utilizadas para transformar las posiciones y las normales del 
+    espacio objeto al esapcio cámara. 
+
+ c) Implementar los métodos:
+
+      setTexture( img )
+      showTexture( show )
+
+    Si la implementación es correcta, podrán visualizar el objeto 3D que hayan cargado y su textura.
+    Notar que los shaders deberán ser modificados entre el ejercicio b) y el c) para incorporar las texturas.
+  
+ d) Implementar los métodos:
+
+      setLightDir(x,y,z)
+      setShininess(alpha)
+    
+    Estas funciones se llaman cada vez que se modifican los parámetros del modelo de iluminación en la 
+    interface. No es necesario transformar la dirección de la luz (x,y,z), ya viene en espacio cámara.
+
+ Otras aclaraciones: 
+
+      * Utilizaremos una sola fuente de luz direccional en toda la escena
+      * La intensidad I para el modelo de iluminación debe ser seteada como blanca (1.0,1.0,1.0,1.0) en RGB
+      * Es opcional incorporar la componente ambiental (Ka) del modelo de iluminación
+      * Los coeficientes Kd y Ks correspondientes a las componentes difusa y especular del modelo 
+        deben ser seteados con el color blanco. En caso de que se active el uso de texturas, la 
+        componente difusa (Kd) será reemplazada por el valor de textura. 
+        
+ <=====================================================================================================> 
+*/
 
 /* Esta función recibe la matriz de proyección (ya calculada), una 
  traslación y dos ángulos de rotación (en radianes). Cada una de 
